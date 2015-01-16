f86594f11d743d16f3d8f741b6f3b39050c1320e
VerenaVertonghen <verena.vertonghen@gmail.com>
2015-01-16 15:43:18 +0100

    First version

--- code/trackerApp1/www/lib/angular-google-maps/src/js/directives/drawing-manager.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  angular.module('uiGmapgoogle-maps').directive("uiGmapDrawingManager", [
-    "uiGmapDrawingManager", function(DrawingManager) {
-      return DrawingManager;
-    }
-  ]);
-
-}).call(this);
